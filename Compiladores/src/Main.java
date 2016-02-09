f78aa34ff34c39415643aa0d0273b1e992e70c4f
jonatha-dionisio <jonatha.dionisio@gmail.com>
2016-02-09 17:55:28 -0300

    Usar código exemplo do postado no forum

--- Compiladores/src/Main.java
+++ Compiladores/src/Main.java
@@ -3,6 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Corpo do Main
+		while(true) {
+      Token t = lexer.next();
+      if (t instanceof EOF) break;
+      System.out.print(t.getText());
+}
 	}
 
 }
