750c7eb299da6e0a1c43c18674ed128b27ddb867
jonatha-dionisio <jonatha.dionisio@gmail.com>
2016-02-10 02:29:37 -0300

    Classe extendida do lexer para tratar comentários

---
+++ Compiladores/src/main/MeuLexico.java
@@ -0,0 +1,48 @@
+package main;
+
+import simpleAdder.lexer.Lexer;
+import simpleAdder.node.*;
+import simpleAdder.lexer.Lexer.State;
+
+
+public class MeuLexico extends Lexer{
+	
+private int count;
+  private TInicioComentarioAninhado comment;
+  private StringBuffer text;
+  // We define a constructor
+  public MeuLexico(java.io.PushbackReader in)
+  { super(in);
+  }
+  // We define a filter that recognizes nested comments.
+  protected void filter()
+  { // if we are in the comment state
+    if(state.equals(State.COMMENT))
+    { // if we are just entering this state
+      if(comment == null)
+      { // The token is supposed to be a comment.
+        // We keep a reference to it and set the count to one
+        comment = (TInicioComentarioAninhado) token;
+        text = new StringBuffer(comment.getText());
+        count = 1;
+        token = null; // continue to scan the input.
+      }
+      else
+      { // we were already in the comment state
+        text.append(token.getText()); // accumulate the text.
+        if(token instanceof TInicioComentarioAninhado)
+          count++;
+        else if(token instanceof TFimComentarioAninhado)
+          count--;
+        if(count != 0)
+          token = null; // continue to scan the input.
+        else
+        { comment.setText(text.toString());
+          token = comment; //return a comment with the full text.
+          state = State.NORMAL; //go back to normal.
+          comment = null; // We release this reference.
+        }
+      }
+    }
+  }
+}
