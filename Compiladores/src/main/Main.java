d71fcc78806cc0e5eaeba58fa965e6f1d89a8b0f
jonatha-dionisio <jonatha.dionisio@gmail.com>
2016-02-11 23:32:13 -0300

    Comitando direto no site

--- Compiladores/src/main/Main.java
+++ Compiladores/src/main/Main.java
@@ -1,16 +1,45 @@
 package main;
 
+import simpleAdder.lexer.Lexer;
+import simpleAdder.lexer.LexerException;
+import simpleAdder.node.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PushbackReader;
 
 public class Main {
-	
+
 public static void main(String[] args) {
-
-	
-	/*while(true) {
-    Token t = lexer.next();
-    if (t instanceof EOF) break;
-    System.out.print(t.getText());
-				}**/
+	if (args.length > 0) {
+		try {
+			MeuLexico lexer = new MeuLexico(new PushbackReader(new FileReader(args[0]), 1024));
+			analisadorLexico(args[0], lexer);
+		} catch (Exception  e) {
+			System.out.println(e);
+		}
+	} else {
+		System.exit(1);
 	}
-
 }
+
+
+public static void analisadorLexico(String arquivo, Lexer lexo)
+	throws LexerException, IOException {
+		Token t = null;
+		int linha = 0;
+		while (!((t = lexo.next()) instanceof EOF)) {
+				if (!(t instanceof TBranco)) {
+					if (linha == 0) {
+						linha = t.getLine();
+						System.out.print("linha " + linha + ": ");
+					}
+					if (linha != t.getLine()) {
+						linha = t.getLine();
+						System.out.print("\nlinha " + linha + ": ");
+					}
+				System.out.print(t.getClass().getSimpleName() + "[\'" + t.getText() + "\'] ");
+			}
+		}
+	}
+}
+
